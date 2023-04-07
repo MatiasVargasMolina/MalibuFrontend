b8907ffc14155b5a7896270741b3b311cb10ea81
michimalonco <vargas.mati1999@gmail.com>
2023-04-07 12:41:21 -0400

    mejora al crear productos

--- src/components/crearProducto.jsx
+++ src/components/crearProducto.jsx
@@ -1,6 +1,14 @@
 
 import React, { useState } from 'react';
 import axios from "axios"
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import FilledInput from '@mui/material/FilledInput';
+import Button from '@mui/material/Button';
+import InputAdornment from '@mui/material/InputAdornment';
+import "./crearProducto.css"
 function CrearProducto() {
   const [nombre, setNombre] = useState('');
   const [descripcion, setDescripcion] = useState('');
@@ -10,41 +18,96 @@ function CrearProducto() {
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    axios.post("http://localhost:8080/productos",{"nombre":nombre,"descripcion":descripcion,"precio":precio,"precioDeEnvio":precioDeEnvio,"imagen":imagen},{withCredentials:true});
+    const data = new FormData(event.currentTarget);
+    axios.post("http://localhost:8080/productos",{"nombre":data.get('nombre'),"descripcion":data.get("Descripcion"),"precio":data.get("precio"),"precioDeEnvio":data.get("precioDeEnvio"),"imagen":data.get("imagen")},{withCredentials:true});
     // Aquí podrías hacer una llamada a una API para crear el producto en tu base de datos
   };
 
   return (
-    <div>
-      <h2>Crear Producto</h2>
+    <div className='contenedor-final-crear'>
       <form onSubmit={handleSubmit}>
-        <label>
-          Nombre:
-          <input type="text" value={nombre} onChange={(event) => setNombre(event.target.value)} />
-        </label>
-        <br />
-        <label>
-          Descripción:
-          <textarea value={descripcion} onChange={(event) => setDescripcion(event.target.value)} />
-        </label>
-        <br />
-        <label>
-          Precio:
-          <input type="number" value={precio} onChange={(event) => setPrecio(event.target.value)} />
-        </label>
-        <br />
-        <label>
-          Precio de envío:
-          <input type="number" value={precioDeEnvio} onChange={(event) => setPrecioDeEnvio(event.target.value)} />
-        </label>
-        <br />
-        <label>
-          URL de la imagen:
-          <input type="text" value={imagen} onChange={(event) => setImagen(event.target.value)} />
-        </label>
-        <br />
-        <br />
-        <button type="submit">Crear</button>
+        <div className='contenedor-crear'>
+        <h2>Crear Producto</h2>
+          <TextField
+                margin="normal"
+                size='small'
+                required
+                fullWidth
+                name="nombre"
+                label="Título de producto"
+                type="nombre"
+                id="nombre"
+                autoComplete="Nombre producto"
+                sx={{
+                  width: 500,
+                  boxShadow: 1,
+                  borderRadius: 2,
+                  p: 2,
+                  minWidth: 300,
+                }}
+              />
+              
+          <TextField
+            id="filled-multiline-static"
+            label="Descripcion de producto"
+            name="Descripcion"
+            multiline
+            rows={4}
+            variant="filled"
+            color="info"
+            sx={{
+              width: 500,
+              boxShadow: 0,
+              borderRadius: 0,
+              p: 2,
+              minWidth: 300,
+            }}
+          
+          />
+                    <TextField name="cantidad" label="Cantidad" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} sx={{
+                  width: 500,
+                  boxShadow: 0,
+                  borderRadius: 2,
+                  p: 2,
+                  minWidth: 300,
+                }} >Cantidad</TextField>
+
+          <TextField name="precio" label="Precio" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} sx={{
+                  width: 500,
+                  boxShadow: 1,
+                  borderRadius: 2,
+                  p: 2,
+                  minWidth: 300,
+                }} >Precio</TextField>
+          <TextField name="precioDeEnvio" label="Precio de envio" inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }} sx={{
+                  width: 500,
+                  boxShadow: 0,
+                  borderRadius: 2,
+                  p: 2,
+                  minWidth: 300,
+                }} >Precio de envio</TextField>
+                  <TextField
+                margin="normal"
+                size='small'
+                required
+                fullWidth
+                name="imagen"
+                label="Url imagen"
+                type="imagen"
+                id="imagen"
+                autoComplete="imagen"
+                sx={{
+                  width: 500,
+                  boxShadow: 1,
+                  borderRadius: 2,
+                  p: 2,
+                  minWidth: 300,
+                }}
+              />
+                      <Button type="submit" variant="contained" color="success">
+        Crear
+      </Button>
+        </div>
       </form>
     </div>
   );
